f7b69e3ed4470ccfeee668299c780ea61f553f02
Jefferson Silva de Lima <74477649+Jeffersonsdlima31203@users.noreply.github.com>
2024-12-30 13:00:40 -0300

    Update amazon.cy.js

--- amazon.cy.js
+++ amazon.cy.js
@@ -12,7 +12,7 @@ describe('template spec', () => {
 
     cy.get('#ap_email').type('jeff.automation1@gmail.com')
     cy.get('.a-button-inner > #continue').click()
-    cy.get('#ap_password').type('Kjp131417!')
+    cy.get('#ap_password').type('###########')
     cy.get('#signInSubmit').click()
 
     cy.get('#ap-account-fixup-phone-skip-link').click()
